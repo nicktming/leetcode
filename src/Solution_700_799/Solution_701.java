a5b50c14cead58d1009ad6f6af806831af35659a
nicktming <2279563278@qq.com>
2018-08-26 11:37:46 +0800

    add leetcode 701 solution

---
+++ src/Solution_700_799/Solution_701.java
@@ -0,0 +1,21 @@
+package Solution_700_799;
+
+import javax.swing.tree.TreeNode;
+
+public class Solution_701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (val < root.val) root.left = insertIntoBST(root.left, val);
+        else root.right = insertIntoBST(root.right, val);
+        return root;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
