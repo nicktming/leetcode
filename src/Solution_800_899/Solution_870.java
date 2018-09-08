967dadf6d6b240b63b5559b47c5000db60aaa9dd
nicktming <2279563278@qq.com>
2018-09-08 13:31:29 +0800

    update leetcode 870 solution

--- src/Solution_800_899/Solution_870.java
+++ src/Solution_800_899/Solution_870.java
@@ -14,10 +14,37 @@ public class Solution_870 {
         }
     }
 
+    // solution from the discussion using pq
+    // time: 85ms
+    // beats: 61.89%
+    public int[] advantageCount(int[] A, int[] B) {
+        int len = A.length;
+        int[] res = new int[len];
+        Arrays.sort(A);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> b[0] - a[0]);
+
+        for (int i = 0; i < len; i++) pq.add(new int[]{B[i], i});
+
+        int lo = 0;
+        int hi = len - 1;
+
+        while (!pq.isEmpty()) {
+            int[] node = pq.poll();
+            int idx = node[1];
+            int val = node[0];
+            if (A[hi] > val) res[idx] = A[hi--];
+            else res[idx] = A[lo++];
+        }
+
+        return res;
+    }
+
+
     // solution from the discussion
     // time: 111ms
     // beats: 26.44%
-    public int[] advantageCount(int[] A, int[] B) {
+    public int[] advantageCount3(int[] A, int[] B) {
 
         // the tree map stores <value, count> pairs of array A
         TreeMap<Integer, Integer> map = new TreeMap<>();
