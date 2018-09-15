a25646e8b7d49b51a946d7ab3873bae49892008c
nicktming <2279563278@qq.com>
2018-09-15 10:50:11 +0800

    update leetcode 901 solution

--- src/Solution_900_999/Solution_901.java
+++ src/Solution_900_999/Solution_901.java
@@ -1,18 +1,61 @@
 package Solution_900_999;
 
+import java.util.Stack;
+
 public class Solution_901 {
 
 
     public static void main(String[] args) {
         StockSpanner ss = new StockSpanner();
-        int[] stocks = {100, 80, 60, 70, 60, 75, 85};
+        int[] stocks = {100, 80, 60, 70, 60, 75, 85, 101};
         for (int stock : stocks) {
             System.out.println(ss.next(stock));
         }
     }
+
+    static class StockSpanner {
+        Stack<int[]> stack = new Stack<int[]>();
+        public StockSpanner() {
+        }
+
+        public int next(int price) {
+            int res = 1;
+            while (!stack.isEmpty() && price >= stack.peek()[0]) {
+                res += stack.pop()[1];
+            }
+            stack.push(new int[]{price, res});
+            return res;
+        }
+    }
+
+    /*
+    version 2
+    Stack
+    static class StockSpanner {
+
+        Stack<int[]> stack = new Stack<int[]>();
+        int index;
+
+        public StockSpanner() {
+
+        }
+
+        public int next(int price) {
+            while (!stack.isEmpty() && price >= stack.peek()[0]) {
+                stack.pop();
+            }
+            int lastIndex = stack.isEmpty() ? -1 : stack.peek()[1];
+            stack.push(new int[]{price, index});
+            //System.out.format("index=%d,lastIndex=%d\n", index, lastIndex);
+            return (index++) - lastIndex;
+        }
+    }
+    */
+
     // version 1
     // time : 152ms
     // beats: 61.64%
+    /*
     static class StockSpanner {
 
         int[][] A = new int[10000][2];
@@ -33,4 +76,5 @@ public class Solution_901 {
             return A[index++][1];
         }
     }
+    */
 }
